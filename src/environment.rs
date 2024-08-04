f649dddbf0308fe9b3b39e8ecf08a0a204883bbd
Piyush Gupta <piyushgupta941d@gmail.com>
2024-08-04 21:45:33 +0530

    changed environment to Rc and refcell

--- src/environment.rs
+++ src/environment.rs
@@ -1,4 +1,6 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 
 use crate::report;
 use crate::{
@@ -6,14 +8,24 @@ use crate::{
     token::{LiteralTypes, Token},
 };
 
+#[derive(Clone)]
 pub struct Environment {
     values: HashMap<String, LiteralTypes>,
+    enclosing: Option<Rc<RefCell<Environment>>>,
 }
 
 impl Environment {
     pub fn new() -> Self {
         Environment {
             values: HashMap::new(),
+            enclosing: None,
+        }
+    }
+
+    pub fn new_with_enclosing(enclosing: Rc<RefCell<Environment>>) -> Self {
+        Environment {
+            values: HashMap::new(),
+            enclosing: Some(enclosing),
         }
     }
 
@@ -24,6 +36,8 @@ impl Environment {
     pub fn get(&self, name: &Token) -> Result<LiteralTypes, RuntimeError> {
         if self.values.contains_key(&name.lexeme) {
             Ok(self.values.get(&name.lexeme).unwrap().clone())
+        } else if self.enclosing.is_some() {
+            Ok(self.enclosing.as_ref().unwrap().borrow().get(name)?)
         } else {
             report(name.line, &format!("Undefined variable '{}'.", name.lexeme));
             Err(RuntimeError {})
@@ -33,6 +47,9 @@ impl Environment {
     pub fn assign(&mut self, name: &Token, value: LiteralTypes) -> Result<(), RuntimeError> {
         if self.values.contains_key(&name.lexeme) {
             self.values.insert(name.lexeme.clone(), value);
+            Ok(())
+        } else if let Some(enclosing) = &self.enclosing {
+            enclosing.borrow_mut().assign(name, value)?;
             Ok(())
         } else {
             report(name.line, &format!("Undefined variable '{}'.", name.lexeme));
