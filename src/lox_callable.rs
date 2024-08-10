9c5c6a80f02866dba17288bcc788646743450e74
Piyush Gupta <piyushgupta941d@gmail.com>
2024-08-10 15:08:56 +0530

    call function implemented for function

--- src/lox_callable.rs
+++ src/lox_callable.rs
@@ -1,35 +1,85 @@
 use crate::{
-    interpreter::{Interpreter, RuntimeError},
+    environment::Environment,
+    interpreter::{Exit, Interpreter},
+    stmt::Function,
     token::LiteralTypes,
 };
+use core::fmt;
+use std::rc::Rc;
 
-#[derive(Debug, Clone, PartialEq)]
 pub enum Callable {
-    Function(Function),
+    Function(LoxFunction),
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Function {}
+impl fmt::Debug for Callable {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Callable")
+    }
+}
+
+impl Clone for Callable {
+    fn clone(&self) -> Self {
+        match self {
+            Callable::Function(lox_function) => Callable::Function(lox_function.clone()),
+        }
+    }
+}
+
+impl PartialEq for Callable {
+    fn eq(&self, other: &Self) -> bool {
+        std::mem::discriminant(self) == std::mem::discriminant(other)
+    }
+}
+
+#[derive(Clone)]
+pub struct LoxFunction {
+    pub declaration: Box<Function>,
+}
 
 pub trait LoxCallable {
     fn call(
         &self,
         interpreter: &mut Interpreter,
         arguments: &[LiteralTypes],
-    ) -> Result<LiteralTypes, RuntimeError>;
+    ) -> Result<LiteralTypes, Exit>;
     fn arity(&self) -> usize;
 }
 
-impl LoxCallable for Function {
+impl LoxFunction {
+    pub fn new(declaration: Function) -> Self {
+        LoxFunction {
+            declaration: Box::new(declaration),
+        }
+    }
+}
+
+impl LoxCallable for LoxFunction {
     fn call(
         &self,
         interpreter: &mut Interpreter,
         arguments: &[LiteralTypes],
-    ) -> Result<LiteralTypes, RuntimeError> {
-        todo!();
+    ) -> Result<LiteralTypes, Exit> {
+        let mut environment = Environment::new_with_enclosing(Rc::clone(&interpreter.globals));
+        for (param, arg) in self.declaration.params.iter().zip(arguments.iter()) {
+            environment.define(param.lexeme.clone(), arg.clone())
+        }
+
+        let i = interpreter.execute_block(&self.declaration.body, environment);
+
+        match &i {
+            Ok(_) => (),
+            Err(e) => {
+                if let Exit::Return(r) = e {
+                    return Ok(r.value.clone());
+                } else {
+                    return Err(Exit::RuntimeError);
+                }
+            }
+        }
+        Ok(LiteralTypes::Nil)
     }
 
     fn arity(&self) -> usize {
-        0
+        self.declaration.params.len()
     }
 }
